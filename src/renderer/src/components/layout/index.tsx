881436df47f79e41248b71d3fcd1326cdd8166c4
MarcioJorgeMelo <marciojorgemedeirosmelofilho@gmail.com>
2025-04-19 11:28:39 -0300

    feat: add Sidebar into Layout

--- src/renderer/src/components/layout/index.tsx
+++ src/renderer/src/components/layout/index.tsx
@@ -1,14 +1,18 @@
 import { Outlet } from 'react-router-dom'
 import { Header } from '../header'
+import * as Collapsible from '@radix-ui/react-collapsible'
+import { Sidebar } from '../sidebar'
 
 export function Layout() {
   return (
-    <>
+    <Collapsible.Root>
+      <Sidebar />
+
       <div className="flex-1 flex flex-col max-w-screen">
         <Header />
 
         <Outlet />
       </div>
-    </>
+    </Collapsible.Root>
   )
 }
