c7cb1e93eb9ce065001ee2971ec8fac675b3cd1a
MarcioJorgeMelo <marciojorgemedeirosmelofilho@gmail.com>
2025-04-19 17:04:37 -0300

    feat: create tray menu

--- src/main/index.ts
+++ src/main/index.ts
@@ -2,6 +2,7 @@ import { app, shell, BrowserWindow, ipcMain } from 'electron'
 import path, { join } from 'node:path'
 import { electronApp, optimizer, is } from '@electron-toolkit/utils'
 import { createFileRoute, createURLRoute } from 'electron-router-dom'
+import { createTray } from './tray'
 
 function createWindow(): void {
   // Create the browser window.
@@ -23,6 +24,8 @@ function createWindow(): void {
       sandbox: false
     }
   })
+
+  createTray(mainWindow)
 
   if (process.platform === 'darwin') {
     const iconPath = path.resolve(__dirname, 'resources', 'icon.png')
