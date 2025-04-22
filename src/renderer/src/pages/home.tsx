7264c374a10859d675e9da1c7f0a1db52129e8ce
MarcioJorgeMelo <marciojorgemedeirosmelofilho@gmail.com>
2025-04-22 16:35:44 -0300

    feat: add a button with action to call fetchUsers

--- src/renderer/src/pages/home.tsx
+++ src/renderer/src/pages/home.tsx
@@ -1,11 +1,21 @@
 import { Link } from 'react-router-dom'
 
 export function Home() {
+  async function handleAdd() {
+    const response = await window.api.fetchUsers()
+
+    console.log(response)
+  }
+
   return (
     <div>
       <h1>Página home</h1>
 
       <Link to="/create">Ir para create</Link>
+      <br />
+      <br />
+
+      <button onClick={handleAdd}>Buscar usuários</button>
     </div>
   )
 }
