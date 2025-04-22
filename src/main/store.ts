8f019fffd954d6b2e49b3882c976e9a489ea53ab
MarcioJorgeMelo <marciojorgemedeirosmelofilho@gmail.com>
2025-04-22 17:12:36 -0300

    feat: create new handle to add new customer into database

--- src/main/store.ts
+++ src/main/store.ts
@@ -2,7 +2,8 @@ import { app, ipcMain } from 'electron'
 import PouchDB from 'pouchdb'
 import path from 'node:path'
 import fs from 'node:fs'
-import { Customer } from '../shared/types/ipc'
+import { randomUUID } from 'node:crypto'
+import { Customer, newCustomer } from '../shared/types/ipc'
 
 let dbPath
 if (process.platform === 'darwin') {
@@ -17,3 +18,23 @@ if (!fs.existsSync(dbDir)) {
 }
 
 const db = new PouchDB<Customer>(dbPath)
+
+async function addCustomer(doc: newCustomer): Promise<PouchDB.Core.Response | void> {
+  const id = randomUUID()
+
+  const data: Customer = {
+    ...doc,
+    _id: id
+  }
+
+  return db
+    .put(data)
+    .then((response) => response)
+    .catch((err) => console.log('Erro ao cadastrar', err))
+}
+
+ipcMain.handle('add-customer', async (event, doc: Customer) => {
+  const result = await addCustomer(doc)
+
+  return result
+})
