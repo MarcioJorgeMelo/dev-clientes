ad86636671281bcda33caac5287a9ac41153dd20
MarcioJorgeMelo <marciojorgemedeirosmelofilho@gmail.com>
2025-04-22 17:14:02 -0300

    feat: create new route API addCustomer that receives a data

--- src/preload/index.ts
+++ src/preload/index.ts
@@ -1,5 +1,6 @@
 import { contextBridge, ipcRenderer } from 'electron'
 import { ElectronAPI, electronAPI } from '@electron-toolkit/preload'
+import { newCustomer } from '../shared/types/ipc'
 
 declare global {
   export interface Window {
@@ -19,7 +20,9 @@ const api = {
   },
   fetchUsers: () => {
     return ipcRenderer.invoke('fetch-users')
-  }
+  },
+  addCustomer: (doc: newCustomer): Promise<void | PouchDB.Core.Response> =>
+    ipcRenderer.invoke('add-customer', doc)
 }
 
 // Use `contextBridge` APIs to expose Electron APIs to
