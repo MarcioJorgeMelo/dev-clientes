9ffcdbb0d1773511752e1f819d33f245c59b744c
MarcioJorgeMelo <marciojorgemedeirosmelofilho@gmail.com>
2025-04-24 18:30:08 -0300

    feat: create About page with data that is version´s app

--- src/renderer/src/pages/about.tsx
+++ src/renderer/src/pages/about.tsx
@@ -1,7 +1,23 @@
+import { useQuery } from '@tanstack/react-query'
+
 export function About() {
+  const { data, isFetching } = useQuery({
+    queryKey: ['version-app'],
+    queryFn: async () => {
+      const response = await window.api.getVersionApp()
+
+      return response
+    }
+  })
+
   return (
-    <div>
-      <h1>Página about</h1>
+    <div className="flex-1 flex flex-col py-12 px-10 text-white">
+      <h1 className="font-semibold text-xl lg:text-3xl">Página sobre</h1>
+
+      <p>
+        Projeto criado no curso <b>@Sujeitoprogramador</b>
+      </p>
+      <p>Versão atual do projeto: {!isFetching && data && data}</p>
     </div>
   )
 }
